32750e00b68df27585469f513f920014239c0276
Carlos Navidad <carlosnavidadp@lemonade.be>
2024-05-16 11:57:27 +0200

    create npx tool

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+#! /usr/bin/env node
 import http from 'http';
 import StompServer from 'stomp-broker-js';
 import express from 'express';
 import cors from 'cors';
 
-const port = process.env.PORT || 3322;
+if (process.argv.includes('--help') || process.argv.includes('-h')) {
+  console.log('[WZ] Options:');
+  console.log('[WZ] -h / --help:    Show command help');
+  console.log('[WZ] -p / --port:    Set the port to run the ws server');
+  process.exit(1);
+}
+
+const getPort = (args) => {
+  const portIndex = args.findIndex((arg) => arg === '--port' || arg === '-p');
+  if (portIndex === -1) {
+    return null;
+  }
+
+  const port = args[portIndex + 1];
+  if (!port) {
+    console.error('[WZ] ERROR:  Port not provided');
+    process.exit(1);
+  }
+
+  if (isNaN(port)) {
+    console.error('[WZ] ERROR: Port must be a number');
+    process.exit(1);
+  }
+
+  if (port < 1024 || port > 49151) {
+    console.error('[WZ] ERROR: Port must be between 1024 and 49151');
+    process.exit(1);
+  }
+
+  return port;
+};
+
+const port = getPort(process.argv) || 3322;
 const app = express();
 app.use(express.json());
 app.use(
@@ -22,14 +55,10 @@ const stompServer = new StompServer({
   protocol: 'ws',
 });
 
-stompServer.subscribe('/lock', (msg) => {
-  console.log('[/lock] ', JSON.parse(msg));
-});
-
 app.post('/event', (req, res) => {
-  console.log('[POST] /event', req.body);
   const { destination, message } = req.body;
-  
+  console.log(`[WZ] Sending message to ${destination}: `, message);
+
   try {
     stompServer.send(destination, null, JSON.stringify(message));
     return res.sendStatus(200);
@@ -41,5 +70,7 @@ app.post('/event', (req, res) => {
 
 app.use('/', express.static('public'));
 
-console.log(`[WS] Websocket url: ws://localhost:${port}/ws`);
-console.log(`[WS] UI url: localhost:${port}`);
+console.log(`[WZ] Websocket url: ws://localhost:${port}/ws`);
+console.log(`[WZ] UI url: localhost:${port}`);
+console.log(``);
+console.log(`[WZ] Server log:`);
